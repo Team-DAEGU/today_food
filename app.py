8db7ff213861d4d5f5c69b40617983ff8d62eee6
thing-zoo <cathy4025@gmail.com>
2023-06-07 20:32:01 +0900

    add category-list get api and show category-list

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 # MongoDB USER ID : daegu / PW : 1234
 client = MongoClient('mongodb+srv://daegu:1234@cluster0.zuemero.mongodb.net/?retryWrites=true&w=majority')
-db = client.TodayFood
+db = client.test
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+# 카테고리별 맛집 리스트 가져오기
+@app.route('/list/<category>')
+def get_list(category):
+    category_list = list(db.test.find({'category': category}, {'_id': False}))
+    return jsonify({'result': category_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
