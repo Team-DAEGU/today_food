623520d44a60901e38a67d858730cee25a80c648
thing-zoo <cathy4025@gmail.com>
2023-06-07 17:34:58 +0900

    add cardview in index.html

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5001, debug=True)
---
+++ static/js/index.js
@@ -0,0 +1,6 @@
+function change_like() {
+    // 클릭한 좋아요의 블럭 id를 가져와서
+    // 좋아요 여부에 따라 변경해주고
+    $(".like").attr("src", "../static/images/heart-fill.png"); // like
+    // 좋아요 데이터 반영하기
+}
