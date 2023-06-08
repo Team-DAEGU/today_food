c0894676565d7ba3d6a81c270e748d5281e51d2f
thing-zoo <cathy4025@gmail.com>
2023-06-08 10:06:52 +0900

    refactor cardview part using jinja2

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    # 전체 맛집 리스트 가져오기
+    all_posts = list(db.test.find({},{'_id':False})) # .sort("created_at", -1) 생성 시간 기준 최신순 정렬 / 게시글 작성 시 created_at 필드 필요!
+    return render_template('index.html', data=all_posts)
 
 # 카테고리별 맛집 리스트 가져오기
 @app.route('/list/<category>')
 def get_list(category):
     category_list = list(db.test.find({'category': category}, {'_id': False}))
-    return jsonify({'result': category_list})
+    return render_template('index.html', data=category_list)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
