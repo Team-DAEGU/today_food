63a60314eaf04086c529907d0c2ccc0c0effc9c6
JIHUN KIM <dksk0359@naver.com>
2023-06-08 13:40:36 +0900

    1st commit

--- app.py
+++ app.py
@@ -8,7 +8,41 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('posts.html')
+
+@app.route('/posts', methods=['POST'])
+def register_post():
+    restaurant_name = request.form['restaurant_name']
+    address = request.form['address']
+    category = request.form['categories']
+    comment = request.form['comment']
+    tags = request.form['tag']
+    img_url=request.form['url']
+    name=request.form['name']
+    password=request.form['password']
+    print(name)
+    print(password)
+    posts_list = list(db.test.find({}, {'_id': False}))
+    num = len(posts_list) + 1
+
+
+        # MongoDB에 데이터 저장
+    post_data = {
+        'num' : num,
+        'title': restaurant_name,
+        'address': address,
+        'category': category,
+        'comment': comment,
+        'tags': tags,
+        'image_url':  img_url,
+        'name':name,
+        'password':password
+    }
+
+    db.test.insert_one(post_data)
+    return jsonify({'msg':'등록이 완료되었습니다.'})
+
+
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5502, debug=True)
