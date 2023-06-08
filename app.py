ca1406c4ae2e825ee01adec9ba02416787337c58
thing-zoo <cathy4025@gmail.com>
2023-06-08 10:56:39 +0900

    update

--- app.py
+++ app.py
@@ -13,18 +13,11 @@ def home():
     all_posts = list(db.test.find({},{'_id':False})) # .sort("created_at", -1) 생성 시간 기준 최신순 정렬 / 게시글 작성 시 created_at 필드 필요!
     return render_template('index.html', data=all_posts)
 
-# 전체 맛집 리스트 가져오기
-@app.route('/index', methods=["GET"])
-def index():
-    all_posts = list(db.test.find({},{'_id':False})) # .sort("created_at", -1) 생성 시간 기준 최신순 정렬 / 게시글 작성 시 created_at 필드 필요!
-    return jsonify({'response': all_posts})
-
 # 카테고리별 맛집 리스트 가져오기
 @app.route('/list/<category>')
 def get_list(category):
     category_list = list(db.test.find({'category': category}, {'_id': False}))
     return render_template('index.html', data=category_list)
 
-
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5001, debug=True)
